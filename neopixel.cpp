d52c94de9f45ef717cc6b979dffc82a9a024114b
Yash <YashdalfTheGray@users.noreply.github.com>
2015-10-24 21:55:49 -0500

    added allOn and allOff functions

---
+++ neopixel.cpp
@@ -0,0 +1,160 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "application.h"
+#include "neopixel/neopixel.h"
+
+SYSTEM_MODE(AUTOMATIC);
+
+/* ======================= prototypes =============================== */
+
+void colorAll(uint32_t c, uint8_t wait);
+void colorWipe(uint32_t c, uint8_t wait);
+void rainbow(uint8_t wait);
+void rainbowCycle(uint8_t wait);
+void allOff();
+uint32_t Wheel(byte WheelPos);
+
+int allOnRemote(String command);
+int allOffRemote(String command);
+
+/* ======================= extra-examples.cpp ======================== */
+
+// IMPORTANT: Set pixel COUNT, PIN and TYPE
+#define PIXEL_COUNT 24
+#define PIXEL_PIN D6
+#define PIXEL_TYPE WS2812B
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+//               note: if not specified, D2 is selected for you.
+// Parameter 3 = pixel type [ WS2812, WS2812B, WS2811, TM1803 ]
+//               note: if not specified, WS2812B is selected for you.
+//               note: RGB order is automatically applied to WS2811,
+//                     WS2812/WS2812B/TM1803 is GRB order.
+//
+// 800 KHz bitstream 800 KHz bitstream (most NeoPixel products ...
+//                         ... WS2812 (6-pin part)/WS2812B (4-pin part) )
+//
+// 400 KHz bitstream (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//                   (Radio Shack Tri-Color LED Strip - TM1803 driver
+//                    NOTE: RS Tri-Color LED's are grouped in sets of 3)
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
+    
+    Particle.function("allOn", allOnRemote);
+    Particle.function("allOff", allOffRemote);
+}
+
+void loop() {
+    // Nothing needs to be done here.
+}
+
+// Set all pixels in the strip to a solid color, then wait (ms)
+void colorAll(uint32_t c, uint8_t wait) {
+    uint16_t i;
+    
+    for(i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+    }
+    strip.show();
+    delay(wait);
+}
+
+// Fill the dots one after the other with a color, wait (ms) after each one
+void colorWipe(uint32_t c, uint8_t wait) {
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+        strip.show();
+        delay(wait);
+    }
+}
+
+void rainbow(uint8_t wait) {
+    uint16_t i, j;
+    
+    for(j=0; j<256; j++) {
+        for(i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel((i+j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout, then wait (ms)
+void rainbowCycle(uint8_t wait) {
+    uint16_t i, j;
+    
+    for(j=0; j<256; j++) { // 1 cycle of all colors on wheel
+        for(i=0; i< strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+void allOff() {
+    for(int i = 0; i<PIXEL_COUNT; i++){
+        strip.setPixelColor(i, strip.Color(0, 0, 0));
+    }
+    strip.show();
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+    if(WheelPos < 85) {
+        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    }
+    else if(WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    else {
+        WheelPos -= 170;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+}
+
+int allOnRemote(String command) {
+    if(command.equals("red")){
+        colorAll(strip.Color(255,0,0), 50);
+    }
+    else if(command.equals("green")){
+        colorAll(strip.Color(0,255,0), 50);
+    }
+    else if(command.equals("blue")){
+        colorAll(strip.Color(0,0,255), 50);
+    }
+    else if(command.equals("white")){
+        colorAll(strip.Color(255,255,255), 50);
+    }
+    else {
+        
+        char inputStr[20];
+        command.toCharArray(inputStr,20);
+        char *p = strtok(inputStr,",");
+        int red = atoi(p);
+        p = strtok(NULL,",");
+        int grn = atoi(p);
+        p = strtok(NULL,",");
+        int blu = atoi(p);
+        p = strtok(NULL,",");
+        colorAll(strip.Color(red,grn,blu), 50);
+    }
+    return 1;
+}
+
+int allOffRemote(String Command) {
+    allOff();
+    return 1;
+}
